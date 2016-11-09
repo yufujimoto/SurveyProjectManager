0ccfac819d79a87678a56aeda63f9652ee0cd56c
yufujimoto <yfujimoto82@gmail.com>
2016-11-09 19:10:54 +0900

    config.php is no longer required.

--- SurveyProjectManager/source/config.php
+++
@@ -1,8 +0,0 @@
-<?php
-	header("Content-Type: text/html; charset=UTF-8");
-    define('UUID', '1119d701-b019-45c5-be5a-10eb4e982661');
-    define('DBUSER', 'yufujimoto');
-    define('DBPASS', 'nevergiveup;0224');
-    define('DBHOST', 'localhost');
-    define('DBNAME', '');
-?>
